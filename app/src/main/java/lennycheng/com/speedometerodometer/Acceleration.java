8b8d689cc6ec4b0f33e1e6a25cf33f7a198b3e66
NanoBreeze <dreambigbebigger@gmail.com>
2016-04-14 21:16:42 -0400

    Refactored and added display

---
+++ app/src/main/java/lennycheng/com/speedometerodometer/Acceleration.java
@@ -0,0 +1,68 @@
+package lennycheng.com.speedometerodometer;
+
+/**
+ * Helper methods for acceleration-related items
+ */
+public class Acceleration {
+
+    public static void filterAccelerations(float[] sensorAccelerations, double[] previousAccelerations, double[] averageAccelerations) {
+        //set offset acceleration values, which are on average between -0.3 to 0.3 to be 0.
+
+        double lowOffset = -0.3;
+        double highOffset = 0.3;
+
+        for (int i = 0; i < 3; i++) {
+
+            if ((lowOffset <= sensorAccelerations[i]) && (sensorAccelerations[i] <= highOffset)) {
+                previousAccelerations[i] = 0;
+                averageAccelerations[i] = 0;
+            } else {
+                previousAccelerations[i] = sensorAccelerations[i];
+            }
+        }
+    }
+
+    public static double[] sanitizePreviousAccelerations(float[] sensorAccelerations) {
+        //set offset acceleration values, which are on average between -0.3 to 0.3 to be 0.
+
+        double[] previousAccelerations = new double[3];
+
+        double lowOffset = -0.3;
+        double highOffset = 0.3;
+
+        for (int i = 0; i < 3; i++) {
+
+            if ((lowOffset <= sensorAccelerations[i]) && (sensorAccelerations[i] <= highOffset)) {
+                previousAccelerations[i] = 0;
+            } else {
+                previousAccelerations[i] = sensorAccelerations[i];
+            }
+        }
+
+        return previousAccelerations;
+    }
+
+
+    public static double[] calculateAverageAccelerations(float[] sensorAccelerations, double[] previousAccelerations) {
+
+        double[] averageAccelerations = new double[3];
+
+        for (int i = 0; i < 3; i++) {
+            averageAccelerations[i] =
+                    (sensorAccelerations[i] + previousAccelerations[i]) / 2;
+        }
+
+        double lowOffset = -0.3;
+        double highOffset = 0.3;
+
+        //sanitize the average acceleration
+        for (int i = 0; i < 3; i++) {
+
+            if ((lowOffset <= sensorAccelerations[i]) && (sensorAccelerations[i] <= highOffset)) {
+                averageAccelerations[i] = 0;
+            }
+        }
+        return averageAccelerations;
+    }
+}
+
